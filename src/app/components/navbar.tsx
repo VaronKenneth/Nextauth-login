5b2a4b5253db0144a5b39ee2a770ecfe2f6201ac
Kafazevc <kafazevc@gmail.com>
2024-04-27 20:59:55 -0500

    first commit

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+export default function Navbar() {
+    return (
+        <nav className="bg-blue-800 p-4">
+            <ul className="flex justify-evenly text-2xl font-bold">
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/dashboard">Dashboard</Link></li>
+                <li><Link href="/api/auth/signout">Sign Out</Link></li>
+                <li><Link href="/api/auth/signin">Sign In</Link></li>
+            </ul>
+        </nav>
+    )
+}
